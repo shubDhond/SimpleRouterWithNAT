70a45e254d3eada45cb31112cb4206f988efbf52
Shubham Dhond <sdhond.sd@gmail.com>
2017-11-22 11:23:53 -0500

    Fixing lab5.py

--- lab5.py
+++ lab5.py
@@ -28,12 +28,12 @@ class CS144Topo( Topo ):
         Topo.__init__( self, *args, **kwargs )
         server1 = self.addHost( 'server1' )
         server2 = self.addHost( 'server2' )
-        nat = self.add_switch( 'sw0' )
-        bridge = self.add_switch( 'sw1' )
+        nat = self.addSwitch( 'sw0' )
+        bridge = self.addSwitch( 'sw1' )
         client = self.addHost('client')
-        self.add_link(client, nat)
+        self.addLink(client, nat)
         for h in server1, server2, nat: #client, root:
-            self.add_link( h,  bridge)
+            self.addLink( h,  bridge)
 
 
 class CS144Controller( Controller ):
