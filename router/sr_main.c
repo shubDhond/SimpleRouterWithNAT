5dec1580870fe6c03e934dc1aacb3037b4336e90
None <None>
2017-11-22 22:08:51 -0800

    Fixed flags

--- router/sr_main.c
+++ router/sr_main.c
@@ -75,7 +75,7 @@ int main(int argc, char **argv)
 
     printf("Using %s\n", VERSION_INFO);
 
-    while ((c = getopt(argc, argv, "hs:v:p:u:t:r:l:T:n:I:E:R")) != EOF)
+    while ((c = getopt(argc, argv, "hs:v:p:u:t:r:l:T:nI:E:R:")) != EOF)
     {
         switch (c)
         {
